a2499d7ff7e79d7d73a91efa55ff384d5e889ea0
ajityadaav <a9860599114@gmail.com>
2026-07-18 10:27:31 +0530

    feat: implement semantic search using cosine similarity

--- 3.EmbeddingModels/2_embedding_openai_docs.py
+++ 3.EmbeddingModels/2_embedding_openai_docs.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from sklearn.metrics.pairwise import cosine_similarity
 
 load_dotenv()
 
@@ -7,14 +8,36 @@ embedding = GoogleGenerativeAIEmbeddings(
     model="models/gemini-embedding-001"
 )
 
+# documents = [
+#     "Delhi is the capital of India",
+#     "Kolkata is the capital of West Bengal",
+#     "Paris is the capital of France"
+# ]
+
+# vectors = embedding.embed_documents(documents)
+
+# print("Number of vectors:", len(vectors))
+# print("Dimension of one vector:", len(vectors[0]))
+# print(vectors[0][:10])   # First 10 values of the first vector
+
 documents = [
-    "Delhi is the capital of India",
-    "Kolkata is the capital of West Bengal",
-    "Paris is the capital of France"
+    "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
+    "MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
+    "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
+    "Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
+    "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
 ]
 
-vectors = embedding.embed_documents(documents)
+query = str(input("Enter your query : "))
 
-print("Number of vectors:", len(vectors))
-print("Dimension of one vector:", len(vectors[0]))
-print(vectors[0][:10])   # First 10 values of the first vector
+doc_embeddings = embedding.embed_documents(documents)
+query_embeddings = embedding.embed_query(query)
+
+scores = cosine_similarity([query_embeddings], doc_embeddings)[0]
+print(scores)
+
+index, score = sorted(list(enumerate(scores)),key=lambda x:x[1])[-1]
+
+print(query)
+print(documents[index])
+print("similarity score is :", score)
